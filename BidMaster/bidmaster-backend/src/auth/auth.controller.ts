552a0c7f495c83bdd40c072e6021121d0b3f4e05
Aleksa <jevticaleksa553@gmail.com>
2026-08-10 14:17:12 +0200

    feat: add auth controller

--- BidMaster/bidmaster-backend/src/auth/auth.controller.ts
+++ BidMaster/bidmaster-backend/src/auth/auth.controller.ts
@@ -1,4 +1,21 @@
-import { Controller } from '@nestjs/common';
+
+import { Body, Controller, Post, HttpCode, HttpStatus } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { CreateUserDto } from 'src/user/dto/create-user.dto';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+    constructor(private readonly authService: AuthService) { }
+
+    @HttpCode(HttpStatus.OK)
+    @Post('login')
+    login(@Body() loginDto: Record<string, any>) {
+        return this.authService.login(loginDto.email, loginDto.password);
+    }
+
+    @HttpCode(HttpStatus.OK)
+    @Post('register')
+    register(@Body() registerDto: CreateUserDto) {
+        return this.authService.register(registerDto);
+    }
+}
