86f351f0d4a955a60ad09a9cec23c86fcff9fdc9
Aleksa <jevticaleksa553@gmail.com>
2026-08-10 20:09:23 +0200

    feat: add guards

---
+++ BidMaster/bidmaster-backend/src/auth/guards/roles.guard.ts
@@ -0,0 +1,30 @@
+import { Injectable, CanActivate, ExecutionContext } from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+import { Role } from '../../user/entities/user.entity';
+import { ROLES_KEY } from '../decorators/roles.decorator';
+
+@Injectable()
+export class RolesGuard implements CanActivate {
+  constructor(private reflector: Reflector) {}
+
+  canActivate(context: ExecutionContext): boolean {
+    const requiredRoles = this.reflector.getAllAndOverride<Role[]>(ROLES_KEY, [
+      context.getHandler(),
+      context.getClass(),
+    ]);
+
+    // ako ruta nema @Roles() dekorator, dozvoli pristup (nema ograničenja uloge)
+    if (!requiredRoles) {
+      return true;
+    }
+
+    const { user } = context.switchToHttp().getRequest();
+
+    // ako nema usera (npr. JwtAuthGuard nije stavljen ispred), odbij
+    if (!user) {
+      return false;
+    }
+
+    return requiredRoles.includes(user.role);
+  }
+}
