c6c968ce46ba10431dbd8e2dbed784cf0d34263b
Aleksa <jevticaleksa553@gmail.com>
2026-08-12 20:52:15 +0200

    feat: add support for cors

--- BidMaster/bidmaster-backend/src/auth/auth.service.ts
+++ BidMaster/bidmaster-backend/src/auth/auth.service.ts
@@ -25,7 +25,13 @@ export class AuthService {
     async login(user: any) {
         const payload = { sub: user.id, email: user.email, role: user.role };
         return {
-            access_token: this.jwtService.sign(payload)
+            access_token: this.jwtService.sign(payload),
+            user: {
+                id: user.id,
+                email: user.email,
+                name: user.name,
+                lastName: user.lastName
+            }
         }
     }
 
